e913c3ad0e69c9b073d87a837c2ce3f69fb419cc
SantiagoLozan <santiagolozan73@gmail.com>
2023-04-25 20:40:09 -0300

    movimiento del personaje

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -11,6 +11,7 @@ export default class Game extends Phaser.Scene {
     this.load.image("square", "./assets/images/square.png");
     this.load.image("diamond", "./assets/images/diamond.png");
     this.load.image("triangle", "./assets/images/triangle.png");
+    this.load.image("meme", "./assets/images/meme.png");
   }
   create() {
     // add background
@@ -18,6 +19,10 @@ export default class Game extends Phaser.Scene {
     //
     let platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    // add meme
+   
+    this.add.image(700, 90,"meme").setScale(0.25);
+    
 
     // add sprite player
     this.player = this.physics.add.sprite(100, 450, "ninja");
@@ -27,6 +32,47 @@ export default class Game extends Phaser.Scene {
     this.shapesGroup.create(100, 0, "diamond");
     this.shapesGroup.create(200, 0, "triangle");
     this.shapesGroup.create(300, 0, "square");
+    this.addShape();
+    //create event to add shapes
+    this.time.addEvent({
+      delay: 1500,
+      callback: this.addShape,
+      callbackScope: this,
+      loop: true,
+    });
+    // create cursor
+    this.cursors= this.input.keyboard.createCursorKeys();
+    //add collierd between player and plataform
+    this.physics.add.collider(this.player, platforms);
+    this.physics.add.collider(this.player, this.shapesGroup);
+    this.physics.add.collider(platforms, this.shapesGroup);
   }
-  update() {}
+  update() {
+    if (this.cursors.left.isDown) {
+  this.player.setVelocityX(-250); 
+} else  {
+  if (this.cursors.right.isDown) {
+  this.player.setVelocityX(250);
+} else{ 
+  this.player.setVelocityX(0);
+}
+
+}
+if (this.cursors.up.isDown && this.player.body.touching.down){
+  this.player.setVelocityY(-330)
+}
+}
+
+
+
+
+addShape (){
+  // get random shape
+  const randomShape = Phaser.Math.RND.pick (["diamond", "square", "triangle"]);
+  //get  random position  x
+  const randomX = Phaser.Math.RND.between(0,800);
+  // get random  to  screen
+  this.shapesGroup.create(randomX, 0, randomShape);
+  console.log("shape is added", randomX, randomShape);
+  }
 }
